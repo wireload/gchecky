b49c5ee943d6ed7dcffe54b3a9ce7611db885e60
etarassov <devnull@localhost>
2007-07-05 00:15:25 +0000

    Rename to gcheckout.

--- samples/django/shop/sql/item.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `shop_item` (`id`, `title`, `description`, `price`, `weight`) VALUES (1, 'Car', 'A cheap Volkswagen.', 17999, 863000);
-INSERT INTO `shop_item` (`id`, `title`, `description`, `price`, `weight`) VALUES (2, 'Flowers', 'Beautiful red roses.', 12, 45);
-INSERT INTO `shop_item` (`id`, `title`, `description`, `price`, `weight`) VALUES (3, 'Notebook', 'Toshiba notebook', 800, 4300);
-
--- samples/django/shop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('shop.views',
-    # Example:
-    (r'^$', 'index'),
-    (r'^notify/$', 'ghandler'),
-    (r'^do/$', 'gdo'),
-)
