41ff7e8e92f90c97bbf2dc7bed3732a0e7667f5c
etarassov <devnull@localhost>
2008-01-03 20:26:23 +0000

    Improve docs.

--- gchecky/__init__.py
+++ gchecky/__init__.py
@@ -14,31 +14,29 @@ the library, which includes the following topics:
     the corresponding hook methods.
 
 Intergration of the library consists of four phases:
-  1. gathering your account settings such as your vendor_id, merchant_key,
+  1. gather your account settings such as your vendor_id, merchant_key,
      your local currency symbol (ATM either 'USD' or 'GBP')
-  2. creating a unique instance of L{Controller} class by subclassing it and
-     providing custom implementation to the abstract methods. Basicly all you
-     need is to define your reaction to various API notifications (see step 5
-     for details).
-  3. setting up a separate background thread for sending requests to U{Google
-     Checkout API <http://code.google.com/apis/checkout/developer/index.html#order_processing_api>}.
-     This could be simply done by installing a cron job which scans your custom
-     system for the orders that should be processed, and sending the
-     corresponding action message to Google Checkout API.
-  4. setting up a web-handler at a publicly accessible url. Refer to the 
+  2. create a unique instance of L{gchecky.ControllerLevel_2} class by
+     subclassing it. Provide custom implementation to the abstract methods.
+     Basically all you need is to define your reaction to various API
+     notifications (see step 5 for details).
+  3. set up a separate background thread for sending requests to
+     U{Google Checkout API <http://code.google.com/apis/checkout/developer/index.html#order_processing_api>}.
+  4. set up a web-handler at a publicly accessible url. Refer to the 
      U{Google Checkout Notification API
      <http://code.google.com/apis/checkout/developer/index.html#handling_notifications>}.
-     But instead of processing incoming XML messages yourself, it should be
-     passed to your customized instance of L{Controller} (its 
-     L{Controller.recieve} method).
+     But instead of processing incoming XML messages yourself, it should
+     pass the incoming request text to your customized instance of
+     L{gchecky.ControllerLevel_2} (to L{gchecky.ControllerLevel_2.recieve}).
   5. Provide your local order-processing system integration by overriding
-     abstract methods of your custom instance of L{Controller} (see steps 2 and 3).
-     Basicly you will need to store Orders in your custom order-processing
+     abstract methods of your custom instance of L{gchecky.ControllerLevel_2}
+     (see steps 2 and 3).
+     Basically you will need to store Orders in your custom order-processing
      system, update it state every time you receive a notification from
      Google Checkout Notification API (notifications correspond to abstract
      Controller.on_XXX methods). Plus in the step 3 you will need to scan your
-     Order list and perform actions on theose orders which are ready for the
-     further processing (or charging, refunding, delivering etc.) 
+     Order list and perform actions on those orders which are ready for the
+     further processing (charging, refunding, delivering etc.) 
 
 @author: etarassov
 @version: $Revision $
