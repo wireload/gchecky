2fb4a4394cf0c7c594089fe2c9df3293dcb6cbc9
etarassov <devnull@localhost>
2008-01-04 11:34:29 +0000

    Add 'build-wininst' target to building script to create win installer.

--- dist.sh
+++ dist.sh
@@ -11,8 +11,16 @@ rm MANIFEST \
    samples/django/gcheckout*.pyc \
    samples/gbutton/*.pyc
 
+# build everything
+python setup.py build
+
 # create source distribution
 python setup.py sdist
 
-# and binary distribution
-python setup.py bdist
+# and binary distributions
+python setup.py bdist bdist_wininst
+
+# TODO: provide 'docs/man/*' for the target 'bdist_rpm'
+
+# clean up
+python setup.py clean
