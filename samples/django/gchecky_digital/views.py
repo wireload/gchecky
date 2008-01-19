fb161dd0fbcb59177ecf046abaee057c7cec3bba
etarassov <devnull@localhost>
2008-01-19 00:20:58 +0000

    Add digital item page. Fix the digital-goods url.

--- samples/django/gchecky_digital/views.py
+++ samples/django/gchecky_digital/views.py
@@ -1,7 +1,17 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic.simple import redirect_to
 from gchecky_common import render_to_response
 from gchecky_common.controller import get_controller
 from gchecky_digital.models import DigitalItem
+
+def make_back_url(request, url):
+    return '%s://%s%s' % ((request.is_secure() and 'https') or 'http',
+                          request.get_host(),
+                          url)
+
+def item_details(request, item_id, template, template_object):
+    item = get_object_or_404(DigitalItem, id=item_id)
+    return render_to_response(template, {template_object:item}, request=request)
 
 def new_order(request, template):
     item_list = DigitalItem.objects.all()
@@ -10,7 +20,7 @@ def new_order(request, template):
 def confirm_order(request, template):
     # TODO: any other prettier way to get an array input parameter value?
     ITEMS_VAR_NAME = 'item_'
-    CONTINUE_SHOPPING_URL = '%s/digital/thanks/' % (request.get_host(),)
+    CONTINUE_SHOPPING_URL = make_back_url(request, '/digital/thanks')
     FRENCH_TAX = 0.196 # In France in general they apply the TAX of 19.6%
     if request.method == 'POST':
         items = []
@@ -42,8 +52,8 @@ def confirm_order(request, template):
                                         description=item.brief,
                                         email_delivery=True,
                                         #TODO: get view url
-                                        url='%s/digital/order/%d/' % (request.get_host(),
-                                                                      item.id,)
+                                        url = make_back_url(request,
+                                                            '/digital/item/%d/' % (item.id,))
                                     )
                                 )
                                 for item in items
@@ -83,5 +93,5 @@ def confirm_order(request, template):
         except:
             raise
     # TODO: get view url
-    return redirect_to(request=request, url=('%s/digital/order/' % (request.get_host(),)))
+    return redirect_to(request=request, url=make_back_url(request, '/digital/order/'))
 
